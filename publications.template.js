9b633efbb58a14374926a22d80d32f3841d94056
Wuqi Aaron Zhang <troublor@live.com>
2021-06-04 16:12:30 +0800

    :bento: add js template

---
+++ publications.template.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "title": "Title of your paper",
+        "date": "date of publication, e.g., 2021-12-31, following ECMA date string format: https://262.ecma-international.org/11.0/#sec-date-time-string-format",
+        "authors": [
+            "First Author", "Second Author"
+        ],
+        "venue": "Full name of the conference or journal name",
+        "venueShort": "abbreviation of conference or journal, e.g., ESEC/FSE",
+        "tags": [
+            "Tag 1",
+            "Tag 2"
+        ],
+        "abstract": "(optional) abstract of your paper",
+        "project": "(optional) project url",
+        "url": "(optional) paper url",
+        "bibtex": `
+            @article{paper
+                ......
+            }
+        `
+    }
+]
